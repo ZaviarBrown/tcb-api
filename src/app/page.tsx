aec7d8a3c94756e6dc4e369ec2dcd6c1b8573862
Zaviar Brown <brown.zaviar@gmail.com>
2024-02-08 04:08:57 -0700

    Apply layout globally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import Link from "next/link";
 
 export default function HomePage() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-                <Link href={"/one-piece"}>One Piece</Link>
-                <Link href={"/jujutsu-kaisen"}>Jujutsu Kaisen</Link>
-            </div>
-        </main>
+        <div className="flex flex-col items-center justify-center gap-12 px-4 py-16">
+            <Link href={"/one-piece"}>One Piece</Link>
+            <Link href={"/jujutsu-kaisen"}>Jujutsu Kaisen</Link>
+        </div>
     );
 }

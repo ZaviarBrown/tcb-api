149f9a0be8c6942214939afae3e35f338ff11fa8
Zaviar Brown <brown.zaviar@gmail.com>
2024-02-08 04:09:09 -0700

    Apply layout globally

--- src/app/jujutsu-kaisen/page.tsx
+++ src/app/jujutsu-kaisen/page.tsx
@@ -17,7 +17,7 @@ export default async function TableOfContents() {
     const chapterList = await getChapterList();
 
     return (
-        <div>
+        <div className="grid grid-cols-3 gap-4 px-4 py-10">
             {chapterList
                 ? chapterList.map((chapter: string, i: number) => {
                       return (
